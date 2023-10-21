a24fd75e5293326655bc62e1eb8a2c79a2ee6bfd
Bastien Leduc <github@bastien-leduc.slmail.me>
2023-10-21 17:38:13 +0200

    Added enemy behavior

---
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private float m_damagePerSecond = 20f;
+    [SerializeField] private float m_searchingDuration = 5f;
+    [SerializeField] private Transform m_startingTransform;
+    [SerializeField] private float m_stoppingDistance = 2f;
+
+    private NavMeshAgent m_navMeshAgent;
+    private const string m_playerTag = "Player";
+
+    Coroutine m_currentSearchingCoroutine;
+
+    private void Start()
+    {
+        m_navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    IEnumerator StartSearching()
+    {
+        yield return new WaitUntil(() => m_navMeshAgent.remainingDistance <= m_stoppingDistance && m_navMeshAgent.hasPath);
+        m_navMeshAgent.isStopped = true;
+        yield return new WaitForSeconds(m_searchingDuration);
+        m_navMeshAgent.isStopped = false;
+
+        m_navMeshAgent.SetDestination(m_startingTransform.position);
+        m_currentSearchingCoroutine = null;
+    }
+
+    public void MoveToPosition(Transform _transform)
+    {
+        if (m_currentSearchingCoroutine != null) return;
+
+        m_navMeshAgent.SetDestination(_transform.position);
+        m_currentSearchingCoroutine = StartCoroutine(StartSearching());
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(m_playerTag) == false) return;
+        m_navMeshAgent.isStopped = true;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag(m_playerTag) == false) return;
+        other.GetComponent<Character>().TakeDamage(m_damagePerSecond * Time.deltaTime);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag(m_playerTag) == false) return;
+        m_navMeshAgent.isStopped = false;
+    }
+}
--- Assets/Scripts/Environment/Electronics.cs
+++ Assets/Scripts/Environment/Electronics.cs
@@ -1,20 +1,25 @@
+using System;
 using UnityEngine;
+using UnityEngine.Events;
 
 [RequireComponent(typeof(Collider))]
 public class Electronics : MonoBehaviour
 {
+    [SerializeField] UnityEvent<Transform> m_triggerCallback;
     private Collider m_collider;
     private const string m_playerTag = "Player";
+    private Transform m_selfTransform;
 
     private void Start()
     {
         m_collider = GetComponent<Collider>();
         m_collider.isTrigger = true;
+        m_selfTransform = transform;
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag(m_playerTag) == false) return;
-        //TODO: TRIGGER AI PATH
+        m_triggerCallback.Invoke(m_selfTransform);
     }
 }
